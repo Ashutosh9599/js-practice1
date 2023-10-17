d68dae31046f61813bf99b0b2051b963268b14d3
Ashutosh9599 <ashutoshdas091@gmail.com>
2023-10-17 19:18:40 +0530

    forth commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello';
-items[2].style.backgroundColor = 'green';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello';
+// items[2].style.backgroundColor = 'green';
 
-for (let i = 0; i < items.length; i++) {
-    items[i].style.fontWeight = 'bold';
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = 'bold';
+// }
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello';
+li[2].style.backgroundColor = 'green';
+
+for (let i = 0; i < li.length; i++) {
+    li[i].style.fontWeight = 'bold';
 }
