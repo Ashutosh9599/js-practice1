9a4811e982e409078586d794dc69dc3dd698ae68
Ashutosh9599 <ashutoshdas091@gmail.com>
2023-10-17 18:28:46 +0530

    third commit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('i love git')
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello';
+items[2].style.backgroundColor = 'green';
+
+for (let i = 0; i < items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+}
