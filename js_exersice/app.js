2f4855d90782351a41f3021b2539d7e31c9d2ef5
Ashutosh9599 <ashutoshdas091@gmail.com>
2023-10-20 14:46:38 +0530

    Thirteenth Commit

--- js_exersice/app.js
+++ js_exersice/app.js
@@ -10,6 +10,8 @@ const userList = document.querySelector('#users');
 myForm.addEventListener('submit', onSubmit);
 // Delete event
 userList.addEventListener('click', removeItem);
+// Delete event
+userList.addEventListener('click', editItem);
 
 
 function onSubmit(e) {
@@ -30,10 +32,10 @@ function onSubmit(e) {
       phone: phoneInput.value
     };
 
-   // Convert the object to a string and store it
+    // Convert the object to a string and store it
     localStorage.setItem(emailInput.value, JSON.stringify(userData));
 
-    
+
     // Create a new list item with user details
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(`${userData.name}: ${userData.email}: ${userData.phone}`));
@@ -44,7 +46,11 @@ function onSubmit(e) {
     deleteButton.className = 'delete';
     li.appendChild(deleteButton);
 
-   
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = 'edit';
+    li.appendChild(editButton);
+
 
     // Add HTML
     // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
@@ -57,21 +63,47 @@ function onSubmit(e) {
     emailInput.value = '';
     phoneInput.value = '';
   }
-  
+
 }
 
 // Remove item
 function removeItem(e) {
   if (e.target.classList.contains('delete')) {
-      if (confirm('Are You Sure?')) {
-          var li = e.target.parentElement;
-          userList.removeChild(li);
+    if (confirm('Are You Sure?')) {
+      var li = e.target.parentElement;
+      userList.removeChild(li);
 
       // Get the email from the list item's text content
       const email = li.textContent.split(':')[1].trim();
 
       // Remove the corresponding data from localStorage
       localStorage.removeItem(email);
-      }
+    }
+  }
+}
+
+// Remove item
+function editItem(e) {
+  if (e.target.classList.contains('edit')) {
+    var li = e.target.parentElement;
+    userList.removeChild(li);
+    // Get the email from the list item's text content
+    const email = li.textContent.split(':')[1].trim();
+     
+     // Call the function to populate input fields
+     dataFromLocalStorage(email);
+
+    // Remove the corresponding data from localStorage
+    localStorage.removeItem(email);
+  }
+}
+
+// Function to populate input fields with data from local storage
+function dataFromLocalStorage(email) {
+  const userData = JSON.parse(localStorage.getItem(email));
+  if (userData) {
+    nameInput.value = userData.name;
+    emailInput.value = userData.email;
+    phoneInput.value = userData.phone;
   }
 }
